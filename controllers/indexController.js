6d161d0521015f5cc82f3fb188e22542d0862ea7
housemouse62 <ryan.skeels@gmail.com>
2026-03-28 23:45:33 +0100

    draft main view css

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,6 +4,7 @@ import db from "../db/queries.js";
 async function viewAllCategoriesGet(req, res) {
   const categories = await db.getAllCategories();
   res.render("index", { title: "All Categories", categories: categories });
+  console.log(categories);
 }
 
 export { viewAllCategoriesGet };
