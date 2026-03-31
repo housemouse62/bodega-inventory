6998c2a4a15843910725f06d87093c9d18d778ce
housemouse62 <ryan.skeels@gmail.com>
2026-03-31 17:51:52 +0200

    add express rate limit

--- routes/itemsRouter.js
+++ routes/itemsRouter.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
 const itemsRouter = Router();
+import rateLimit from "express-rate-limit";
+const deleteLimiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 10 });
+
 import {
   viewItemDetailsGet,
   viewAllItemsGet,
@@ -14,7 +17,7 @@ itemsRouter.get("/", viewAllItemsGet); // view list of all items? or grid?
 itemsRouter.get("/new", addItemShowForm);
 itemsRouter.post("/new", addItem);
 itemsRouter.get("/:id/confirmDeleteItem", confirmDeleteItem); // popup for password to confirm item deletion
-itemsRouter.post("/:id/deleteItem", deleteItem); // deletion confirmation message
+itemsRouter.post("/:id/deleteItem", deleteLimiter, deleteItem); // deletion confirmation message
 itemsRouter.post("/:id/edit", updateItem);
 itemsRouter.get("/:id", viewItemDetailsGet); //view individual item details
 export default itemsRouter;
